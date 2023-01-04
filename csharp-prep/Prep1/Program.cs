a92a732067fb124802e1b81772b0e9774c8392a7
Finch-75 <williamchadfinch@gmail.com>
2023-01-04 08:22:10 -0700

    Finished prep 1 code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        
+
+        Console.WriteLine();
+        /// Ask for first name.
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        /// Ask for last name.
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine();
+        /// Give your name, James Bond style.
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
+        Console.WriteLine();
+
+
+    
+
     }
 }

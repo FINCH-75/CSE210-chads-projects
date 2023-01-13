e264bc2d6eed3a7db749187491671916ae4fd45c
Finch-75 <williamchadfinch@gmail.com>
2023-01-13 14:36:57 -0700

    Week 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+        
+        while (guess != magicNumber)
+        {
+            Console.WriteLine ("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            
+            
+            if (magicNumber > guess)
+            {
+              Console.WriteLine("Higher");      
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("Congrats you guessed it!");
+            }
+    
+        }
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,57 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcomemessage();
+        string userName = UserNamePrompt();
+        int userNumber = FavNumberPrompt();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+
+
     }
+
+    static void DisplayWelcomemessage()
+    {
+        Console.WriteLine("Welcome to my program!");
+    }
+
+
+    static string UserNamePrompt()
+    {
+        Console.Write("Please enter your full name.");
+        string name = Console.ReadLine();
+        
+        return name;
+    }
+
+    static int FavNumberPrompt()
+    {
+        Console.Write("What is your favorite number?");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, your number squared is {square}");
+    }
+
+
+
+
+
+
+
+
 }
